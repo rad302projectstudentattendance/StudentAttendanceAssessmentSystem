730e91db6420541b922d709512cfa4715cc568ac
Nova <31923394+S00173244@users.noreply.github.com>
2019-03-14 15:48:14 +0000

    Added DTO Classes ( Might get changed later )

---
+++ StudentAALibrary/StudentAAWebApi/Models/DTO/StudentDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAAWebApi.Models.DTO
+{
+    public class StudentDTO
+    {
+        public int ID { get; set; }
+
+        public string StudentID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
