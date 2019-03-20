ef45f18ee6237fe4699ae2920ee37057c8108dc2
fdavey22 <S00170902@mail.itsligo.ie>
2019-03-20 13:47:18 +0000

    Added more repositories

--- StudentAALibrary/StudentAAWebApi/DAL/IStudentGradeRepository.cs
+++ StudentAALibrary/StudentAAWebApi/DAL/IStudentGradeRepository.cs
@@ -7,9 +7,9 @@ using StudentAAWebApi.Models.DTO;
 
 namespace StudentAAWebApi.DAL
 {
-    interface ILecturerRepository: IDisposable
+    interface IStudentGradeRepository
     {
-        IEnumerable<LecturerDTO> GetLecturers();
-        LecturerDTO GetLecturerByID(int lecturerID);
+        IEnumerable<StudentGradeDTO> GetStudentGrades();
+        StudentGradeDTO GetStudentGradeByID(int studentGradeID);
     }
 }
