89a1a88c23a20a17e3a3985d640a79062b739ac1
fdavey22 <S00170902@mail.itsligo.ie>
2019-03-20 12:57:34 +0000

    Started Repositories

---
+++ StudentAALibrary/StudentAAWebApi/DAL/ILecturerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentAAWebApi.Models.DTO;
+
+namespace StudentAAWebApi.DAL
+{
+    interface ILecturerRepository: IDisposable
+    {
+        IEnumerable<LecturerDTO> GetLecturers();
+        LecturerDTO GetLecturerByID(int LecturerID);
+    }
+}
---
+++ StudentAALibrary/StudentAAWebApi/DAL/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StudentAAWebApi.Models.DTO;
+
+namespace StudentAAWebApi.DAL
+{
+    interface IStudentRepository: IDisposable
+    {
+        IEnumerable<StudentDTO> GetStudents();
+        StudentDTO GetStudentByID(int studentID);
+
+    }
+}
